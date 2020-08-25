489dfdc4a571acc6bcd13e3dacfdd62c915d93b2
Trevor James <trevorwillemjames@gmail.com>
2020-08-25 12:12:34 -0700

    Postgres DB created

--- app/api/postgres_db.py
+++ app/api/postgres_db.py
@@ -2,45 +2,45 @@ import psycopg2
 import pandas as pd
 from psycopg2.extras import execute_values
 
-df = pd.read_csv('cannabis.csv')
+df = pd.read_csv('cannabis_new.csv')
 
 # cleaning
 
 dbname = 'zgexitff'
 user = 'zgexitff'
-password = 'XXX'
+password = 'N-rZTbhw5RUyDylzQH6Cmai2wSD4SGtr'
 host = 'isilo.db.elephantsql.com'
 
 # connect to DB
 pg_conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
 
-#cursor
+# cursor
 pg_curs = pg_conn.cursor()
 
-create_cannabis_table ="""
-DROP TABLE IF EXIST Cannabis;
+create_cannabis_table = """
+DROP TABLE IF EXISTS Cannabis;
 CREATE TABLE Cannabis (
     index INT, 
-    strain TEXT,
-    rating, 
-    type TEXT, 
-    effects TEXT, 
-    description TEXT,
+    Strain TEXT,
+    Type TEXT, 
+    Rating REAL, 
+    Effects TEXT, 
+    Description TEXT,
+    Flavors TEXT, 
+    Nearest TEXT
+);    
 """
 
 # creating table in postgres
-pg_curs.excute(create_cannabis_table)
+pg_curs.execute(create_cannabis_table)
 pg_conn.commit()
 
 # execute values function
 execute_values(pg_curs, """
 INSERT INTO Cannabis
-(strain, rating, type, effects, description)
+(index, Strain, Type, Rating, Effects, Description, Flavors, Nearest)
 VALUES %s; 
 """, [tuple(row) for row in df.values])
 
 # commit
 pg_conn.commit()
-
-
-
