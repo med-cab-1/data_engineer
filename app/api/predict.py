68fdcaaa4fde228b0a4ae1474f1735d6ccdae61a
Trevor James <trevorwillemjames@gmail.com>
2020-08-26 10:07:17 -0700

    prediction function needs model + input from web team

--- app/api/predict.py
+++ app/api/predict.py
@@ -10,67 +10,27 @@ log = logging.getLogger(__name__)
 router = APIRouter()
 
 """
-Code below is in progress, currently coommented out to prevent errors
+Code below is in progress, currently commented out to prevent errors
 when deploying to Heroku
 """
 
 
-# class Item(BaseModel):
-#     """Use this data model to parse the request body JSON."""
-#
-#     x1: float = Field(..., example=3.14)
-#     x2: int = Field(..., example=-42)
-#     x3: str = Field(..., example='banjo')
-#
-#     def to_df(self):
-#         """Convert pydantic object to pandas dataframe with 1 row."""
-#         return pd.DataFrame([dict(self)])
-#
-#     @validator('x1')
-#     def x1_must_be_positive(cls, value):
-#         """Validate that x1 is a positive number."""
-#         assert value > 0, f'x1 == {value}, must be > 0'
-#         return value
-#
-#
-# @router.post('/predict')
-# async def predict(item: Item):
-#     """
-#     Drop down menus for the Following:
-#
-#     1- Bodily Affects: Tingly, Energetic, Relaxed
-#     2- Mind affects: Happy, Euphoric,
-#     3- Mood alteration: Uplifted, Giggly, Focused
-#     4- Taste: Earthy, Woody, Pine, Grape, Sweet, Pungent
-#     5- THC:CBD Ratio: 1:1, 1:10, 1:20, 3:1, 5:1, 10:1
-#     """
-#
-#     x_df = item.to_df()
-#     y_pred = 'OG Kush'  # ML model would output the most desirable strain here
-#     y_pred_proba = 'OG Kush matches your desired affect to 98% accuracy'  # here we would describe how closely it fits
-#     return {
-#         'prediction': y_pred,
-#         'probability': y_pred_proba
-#         # 'Hello, World!''
-#     }
+class Input(BaseModel):
+    """Use this data model to parse the request body JSON."""
+    input_phrase: str
 
-@router.get('/predict')
-async def test_prediction():
+
+@router.post('/predict')
+async def test_prediction(user_input: Input):
     conn = sqlite3.connect('cannabis.sqlite3')
     curs = conn.cursor()
-    pred = 25  # Stable prediction before the model goes into place
-    # pred = model.predict(input)
-    query_strain = curs.execute(f"SELECT * FROM Cannabis WHERE Strain_ID == {pred}")
+    pred = 687  # Stable prediction before the model goes into place
+    # pred = model.predict(user_input.input_phrase)
+    query_strain = curs.execute(f"SELECT * FROM Cannabis WHERE Strain_ID == {pred} ORDER BY Rating")
     strain = curs.fetchall()
-    demo_response = {
-        'strain': {
-            'name': 'Grandaddy Purple',
-            'type': 'Indica',
-            'description': 'Makes you sleep',
-            'Terpenes': ['Herbal', 'Peppery', 'Pine'],
-            'Effect': 'Calming'
-        }
-    }
+    keys = ['ID', 'Strain_id', 'Name', 'Type', 'Rating', 'Effects', 'Description', 'Flavors', 'Neighbors']
+    suggestion = {k: v for k, v in zip(keys, strain[0])}
+    for key in ['Effects', 'Flavors', 'Neighbors']:
+        suggestion[key] = suggestion[key].split(',')
 
-    return JSONResponse(content=strain)
-    # return JSONResponse(content=strain)
+    return JSONResponse(content=suggestion)
