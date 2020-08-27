4f8c4b237457cf5609e2a66f11f12318ae8d1ccf
JeremySpradlin <spradlinjk@gmail.com>
2020-08-27 11:39:19 -0400

    MVP

--- app/api/predict.py
+++ app/api/predict.py
@@ -7,6 +7,7 @@ from fastapi.responses import JSONResponse
 import pandas as pd
 from pydantic import BaseModel, Field, validator
 from .init_db import create_db, say_hi
+from .model import train
 
 log = logging.getLogger(__name__)
 router = APIRouter()
@@ -27,14 +28,29 @@ async def test_prediction(user_input: Input):
     # conn = sqlite3.connect('../../Data/cannabis.sqlite3')
     conn = sqlite3.connect('Data/cannabis.sqlite3')
     curs = conn.cursor()
-    pred = 687  # Stable prediction before the model goes into place
+    # pred = 687  # Stable prediction before the model goes into place
     # pred = model.predict(user_input.input_phrase)
+    print(user_input.input_phrase)
+    pred = train(user_input.input_phrase)
+
+    # strains = []
+    # for p in pred:
+    #     query_strain = curs.execute(f"SELECT * FROM Cannabis WHERE Strain_ID == {p} ORDER BY Rating")
+    #     strain = curs.fetchall()
+    #     keys = ['ID', 'Strain_id', 'Name', 'Type', 'Rating', 'Effects', 'Description', 'Flavors', 'Neighbors']
+    #     suggestion = {k: v for k, v in zip(keys, strain[0])}
+    #     for key in ['Effects', 'Flavors', 'Neighbors']:
+    #         suggestion[key] = suggestion[key].split(',')
+    #     strains.append(suggestion)
+
     query_strain = curs.execute(f"SELECT * FROM Cannabis WHERE Strain_ID == {pred} ORDER BY Rating")
     strain = curs.fetchall()
     keys = ['ID', 'Strain_id', 'Name', 'Type', 'Rating', 'Effects', 'Description', 'Flavors', 'Neighbors']
     suggestion = {k: v for k, v in zip(keys, strain[0])}
     for key in ['Effects', 'Flavors', 'Neighbors']:
         suggestion[key] = suggestion[key].split(',')
+
+
 
     return JSONResponse(content=suggestion)
 
@@ -43,9 +59,9 @@ async def test_prediction(user_input: Input):
 async def init_db():
     create_db()
 
-@router.get('/hi')
+@router.get('/test')
 async def hello():
-    say_hi()
+    train()
 
 
 if __name__ == '__main__':
